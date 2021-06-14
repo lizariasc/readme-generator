363db234e59e50ca5248f8a5969e7d31f98f43a5
LAgrl <lizethariasc14@gmail.com>
2021-06-13 19:33:09 -0700

    user questions

---
+++ index.js
@@ -0,0 +1,71 @@
+// Packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// Questions for user input
+const questions = [
+{
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?"
+ },
+ {
+    type: "input",
+    name: "description",
+    message: "Provide a description of the project" 
+ },
+ {
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions of your project? Provide a detail step-by-step description" 
+ },
+ {
+    type: "input",
+    name: "usage",
+    message: "What is the purpose of your project?" 
+ },
+ {
+    type: "list",
+    name: "license",
+    message: "What type of license would you like to add to your project?",
+    choices: [
+        "MIT",
+        "Apache",
+        "ISC",
+        "Mozilla Public Liense",
+        "Open",
+        "GNU"
+    ] 
+},
+{
+    type: "input",
+    name: "contributing",
+    message: "What guidelines should people follow when contributing to yout project?" 
+ },
+ {
+    type: "input",
+    name: "test",
+    message: "What test should be ran on your project? Provide detail instructions" 
+ },
+ {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?" 
+ },
+ {
+    type: "input",
+    name: "email",
+    message: "What is your email?" 
+ },
+];
+
+// function to write README file
+const writeToFile = (fileName, data) => {
+
+}
+
+// function to initialize app
+const init = () => {}
+
+// Function call to initialize app
+init();
